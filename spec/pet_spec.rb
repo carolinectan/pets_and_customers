0852641897f9d9e1ccb90d0eff264b0920891e12
Caroline Tan <carolinectan3@gmail.com>
2021-05-27 20:44:35 -0400

    Finish tests for pets

---
+++ spec/pet_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/pet'
+
+RSpec.describe Pet do
+  before :each do
+    @samson = Pet.new({
+      name: "Samson",
+      type: :dog,
+      age: 3
+    })
+  end
+
+  it "exists" do
+    expect(@samson).to be_a(Pet)
+  end
+
+  it "initializes with attributes" do
+    expect(@samson.name).to eq("Samson")
+    expect(@samson.type).to eq(:dog)
+    expect(@samson.age).to eq(3)
+  end
+
+  it "can evalutate if the pet has been fed" do
+    expect(@samson.fed?).to eq(false)
+
+    @samson.feed
+
+    expect(@samson.fed?).to eq(true)
+  end
+end

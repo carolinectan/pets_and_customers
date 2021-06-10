ef44f25b0d0c9edf8ad7a2adfc8c4d12127f5a8f
Caroline Tan <carolinectan3@gmail.com>
2021-06-09 22:01:31 -0400

    Change double quotes to single

--- spec/groomer_spec.rb
+++ spec/groomer_spec.rb
@@ -5,50 +5,50 @@ require './lib/groomer'
 
 RSpec.describe Groomer do
   before :each do
-    @megan = Customer.new("Megan", 5462)
-    @leia = Pet.new({name: "Leia", type: :dog, age: 4})
+    @megan = Customer.new('Megan', 5462)
+    @leia = Pet.new({name: 'Leia', type: :dog, age: 4})
     @megan.adopt(@leia)
 
-    @joni = Customer.new("Joni", 9486)
-    @bosco = Pet.new({name: "Bosco", type: :dog, age: 8})
+    @joni = Customer.new('Joni', 9486)
+    @bosco = Pet.new({name: 'Bosco', type: :dog, age: 8})
     @joni.adopt(@bosco)
 
-    @wagstoriches = Groomer.new("Wags to Riches", [@megan, @joni])
+    @wagstoriches = Groomer.new('Wags to Riches', [@megan, @joni])
 
-    @carey = Customer.new("Carey", 8459)
-    @chicken = Pet.new({name: "Chicken", type: :cat, age: 10})
-    @squirrel = Pet.new({name: "Squirrel", type: :cat, age: 7})
+    @carey = Customer.new('Carey', 8459)
+    @chicken = Pet.new({name: 'Chicken', type: :cat, age: 10})
+    @squirrel = Pet.new({name: 'Squirrel', type: :cat, age: 7})
     @carey.adopt(@chicken)
     @carey.adopt(@squirrel)
 
-    @john = Customer.new("John", 3249)
-    @jack = Pet.new({name: "Jack", type: :dog, age: 2})
+    @john = Customer.new('John', 3249)
+    @jack = Pet.new({name: 'Jack', type: :dog, age: 2})
     @john.adopt(@jack)
 
-    @scruffytofluffy = Groomer.new("Scruffy to Fluffy", [@carey, @john])
+    @scruffytofluffy = Groomer.new('Scruffy to Fluffy', [@carey, @john])
   end
 
-  it "exists" do
+  it 'exists' do
     expect(@wagstoriches).to be_a(Groomer)
     expect(@scruffytofluffy).to be_a(Groomer)
   end
 
-  it "initializes with attributes" do
-    expect(@wagstoriches.name).to eq("Wags to Riches")
+  it 'initializes with attributes' do
+    expect(@wagstoriches.name).to eq('Wags to Riches')
     expect(@wagstoriches.customers).to eq([@megan, @joni])
 
-    expect(@scruffytofluffy.name).to eq("Scruffy to Fluffy")
+    expect(@scruffytofluffy.name).to eq('Scruffy to Fluffy')
     expect(@scruffytofluffy.customers).to eq([@carey, @john])
   end
 
-  it "can return pets of each customer" do
+  it 'can return pets of each customer' do
     expect(@megan.pets).to eq([@leia])
     expect(@joni.pets).to eq([@bosco])
     expect(@carey.pets).to eq([@chicken, @squirrel])
     expect(@john.pets).to eq([@jack])
   end
 
-  it "can determine customers with outstanding balances" do
+  it 'can determine customers with outstanding balances' do
     @joni.charge(30)
 
     expect(@megan.outstanding_balance).to eq(0)
@@ -63,7 +63,7 @@ RSpec.describe Groomer do
     expect(@scruffytofluffy.customers_with_outstanding_balances).to eq([@carey.name, @john.name])
   end
 
-  it "can count the number of pets of a given type" do
+  it 'can count the number of pets of a given type' do
     expect(@wagstoriches.count_pets_by_type(:dog)).to eq(2)
     expect(@wagstoriches.count_pets_by_type(:cat)).to eq(0)
 
